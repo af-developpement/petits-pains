10a7ca5c8b39b97c4154199488a5d9cb525db314
François <alexandre.s@toneofirst.com>
2024-12-26 14:52:29 +0100

    Ajout pour workflow

---
+++ migrations/Version20241226132515.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241226132515 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE participant (id INT AUTO_INCREMENT NOT NULL, prenom VARCHAR(50) NOT NULL, nom VARCHAR(50) DEFAULT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tirage (id INT AUTO_INCREMENT NOT NULL, participant_id INT NOT NULL, date_tirage DATETIME NOT NULL, INDEX IDX_2A145AFF9D1C3019 (participant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE tirage ADD CONSTRAINT FK_2A145AFF9D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tirage DROP FOREIGN KEY FK_2A145AFF9D1C3019');
+        $this->addSql('DROP TABLE participant');
+        $this->addSql('DROP TABLE tirage');
+    }
+}
