0f7507d4c504f3f19fc0ea77d1fdb1e64096a3fe
François <alexandre.s@toneofirst.com>
2024-12-27 18:37:08 +0100

    Ajout tests

--- migrations/Version20241226132515.php
+++ migrations/Version20241226132515.php
@@ -20,8 +20,8 @@ final class Version20241226132515 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE IF NOT EXISTS participant (id INT AUTO_INCREMENT NOT NULL, prenom VARCHAR(50) NOT NULL, nom VARCHAR(50) DEFAULT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE IF NOT EXISTS tirage (id INT AUTO_INCREMENT NOT NULL, participant_id INT NOT NULL, date_tirage DATETIME NOT NULL, INDEX IDX_2A145AFF9D1C3019 (participant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE participant (id INT AUTO_INCREMENT NOT NULL, prenom VARCHAR(50) NOT NULL, nom VARCHAR(50) DEFAULT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tirage (id INT AUTO_INCREMENT NOT NULL, participant_id INT NOT NULL, date_tirage DATETIME NOT NULL, INDEX IDX_2A145AFF9D1C3019 (participant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE tirage ADD CONSTRAINT FK_2A145AFF9D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id)');
     }
 
